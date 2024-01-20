bd31d19c278e985342db671f4d7160acf441bf99
Kamillo Ferry <kafe@kafe.dev>
2024-01-21 00:35:51 +0100

    Add optional parameters to example

--- examples/intro.jl
+++ examples/intro.jl
@@ -3,4 +3,5 @@ using Polytropes
 
 G = complete_dag(3)
 I = toric_ideal(G)
-GF = groebner_fan(I)
+GF,gbs = groebner_fan(I; return_groebner_bases=true)
+# GF,gbs,initial_ideals = groebner_fan(I; return_groebner_bases=true,return_initial_ideals=true)
