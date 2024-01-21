141668ee7fb5652a85d1ac90252c62315eedc5d0
Kamillo Ferry <kafe@kafe.dev>
2024-01-21 02:56:20 +0100

    Fix a typo

--- examples/intro.jl
+++ examples/intro.jl
@@ -4,4 +4,4 @@ using Polytropes
 G = complete_dag(3)
 I = toric_ideal(G)
 GF,gbs = groebner_fan(I; return_groebner_bases=true)
-# GF,gbs,initial_ideals = groebner_fan(I; return_groebner_bases=true,return_initial_ideals=true)
+# GF,gbs,ords,initial_ideals = groebner_fan(I; return_groebner_bases=true,return_orderings=true,return_initial_ideals=true)
