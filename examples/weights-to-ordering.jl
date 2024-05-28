0190b82fb4fc30ac6c08baa01463a5a835a653c4
Kamillo Ferry <kafe@kafe.dev>
2024-05-28 11:53:06 +0200

    Add some code to go from fans to polytropes

--- examples/weights-to-ordering.jl
+++ examples/weights-to-ordering.jl
@@ -1,14 +1,26 @@
 using Oscar
 using Polytropes
 
-G = complete_dag(3)
+G = complete_dag(4)
 R = edge_ring(G)
+o = default_ordering(R)
 I = toric_ideal(G)
 
+GF = groebner_fan(I)
+
+w = -Vector.(maximal_cones(GF) .|> rays_modulo_lineality .|> first .|> sum)
+Q = weighted_digraph_polyhedron.(Ref(G), w)
+
+Vector{Int}(v::RayVector{QQFieldElem}) = Vector{Int}(v.//gcd(v))
+oW = weight_ordering.(Vector{Int}.(w),Ref(o))
+
+F = kleene_polynomials(G)
+inF = oW .|> w->leading_term.(F; ordering=w) .|> unique
+
 # w = [-1,-2,-3]
-w = [-1,-3,-2]
-
-o = lex(R)
-oW = weight_ordering(w,o)
-
-leading_ideal(I;ordering=oW)
+#w = [-1,-3,-2]
+#
+#o = default_ordering(R)
+#oW = weight_ordering(w,o)
+#
+#leading_ideal(I;ordering=oW)
