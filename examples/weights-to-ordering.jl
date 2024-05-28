43a0d79476e91fa64d6b53237562ea63b542fce2
Kamillo Ferry <ferry@math.tu-berlin.de>
2024-05-28 13:01:09 +0200

    Fix some errors in my example

--- examples/weights-to-ordering.jl
+++ examples/weights-to-ordering.jl
@@ -11,16 +11,9 @@ GF = groebner_fan(I)
 w = -Vector.(maximal_cones(GF) .|> rays_modulo_lineality .|> first .|> sum)
 Q = weighted_digraph_polyhedron.(Ref(G), w)
 
-Vector{Int}(v::RayVector{QQFieldElem}) = Vector{Int}(v.//gcd(v))
+Vector{Int}(v::Vector{QQFieldElem}) = Int.(v./gcd(v))
 oW = weight_ordering.(Vector{Int}.(w),Ref(o))
 
 F = kleene_polynomials(G)
-inF = oW .|> w->leading_term.(F; ordering=w) .|> unique
+inF = oW .|> (w->leading_term.(F; ordering=w)) .|> unique
 
-# w = [-1,-2,-3]
-#w = [-1,-3,-2]
-#
-#o = default_ordering(R)
-#oW = weight_ordering(w,o)
-#
-#leading_ideal(I;ordering=oW)
