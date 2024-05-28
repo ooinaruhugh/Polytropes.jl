30cbfd1efe6498c8e854ca338200b7955c9ead69
Kamillo Ferry <kafe@kafe.dev>
2024-05-28 13:09:03 +0200

    Add a polytrope function

--- src/weighted_digraph_polyhedron.jl
+++ src/weighted_digraph_polyhedron.jl
@@ -3,7 +3,19 @@ import Oscar: ZZRingElem, RayVector, QQFieldElem
 
 Vector{ZZRingElem}(v::RayVector{QQFieldElem}) = ZZ.(convert(Vector, v))
 
+function polytrope(G::Graph{Directed}, w::AbstractVector)
+  n = number_of_vertices(G)
+  A = sparse_matrix(QQ)
+
+  for e in edges(G)
+    push!(A, sparse_row(QQ, [(src(e), -1), (dst(e),1)]))
+  end
+
+  return polyhedron(MatElem(A), w)
+end
+
 function weighted_digraph_polyhedron(G::Graph{Directed}, w)
+    # This is already happening in the tropical projective torus
     n = number_of_vertices(G) - 1
     A = []
 
