e4d79558831c72ac429f0e385912e0b1ce900461
Kamillo Ferry <kafe@kafe.dev>
2024-06-07 01:17:59 +0200

    Add functions for tropical hypersurfaces and dual subdivisions

--- src/Cayley.jl
+++ src/Cayley.jl
@@ -73,9 +73,7 @@ end
 function inverse_cayley_embedding(C::AbstractVector{<:PointVector}, n::Int)
     A_with_j = map(x->(x[1:end-n], findfirst(==(1), x[end+1-n:end])), C)
 
-    return map.(Ref(first), 
-             filter(x->last(x)==j, A_with_j) for j in 1:n
-            )
+    return map.(Ref(first), filter(x->last(x)==j, A_with_j) for j in 1:n)
 end
 
 @doc raw"""
@@ -86,7 +84,9 @@ calculates the corresponding mixed subdivision of the Minkowski sum of the point
 as incidence matrix.
 """
 function minkowski_projection(::Type{IncidenceMatrix}, S::SubdivisionOfPoints, n::Int; M=nothing)
-    return minkowski_projection(points(S), maximal_cells(IncidenceMatrix, S), n; M=M)
+    return IncidenceMatrix.(
+        minkowski_projection(points(S), maximal_cells(IncidenceMatrix, S), n; M=M)
+    )
 end
 
 @doc raw"""
@@ -100,16 +100,14 @@ function minkowski_projection(S::SubdivisionOfPoints, n::Int; M=nothing)
         C = points(S)
         A_with_j = map(x->(x[1:end-n], findfirst(==(1), x[end+1-n:end])), C)
 
-        A = map.(Ref(first), 
-                 filter(x->last(x)==j, A_with_j) for j in 1:n
-                )
+        A = map.(Ref(first), filter(x->last(x)==j, A_with_j) for j in 1:n)
         
         M = minkowski_sum(A...)
     end
 
     return subdivision_of_points(
-                 reduce(hcat, M) |> transpose, 
-                 minkowski_projection(IncidenceMatrix, S, n; M=M)
+             reduce(hcat, M) |> transpose, 
+             minkowski_projection(IncidenceMatrix, S, n; M=M)
            )
 end
 
@@ -120,19 +118,17 @@ For a subdivision given by `cells` of the Cayley embedding `C` of `n` point sets
 calculates the corresponding mixed subdivision of the Minkowski sum.
 """
 function minkowski_projection(
-        C::AbstractVector{<:PointVector},
-        cells::IncidenceMatrix,
-        n::Int;
-        M=nothing,
-        labels=nothing
-    )
+    C::AbstractVector{<:PointVector},
+    cells::IncidenceMatrix,
+    n::Int;
+    M=nothing,
+    labels=nothing
+)
 
     A_with_j = map(x->(x[1:end-n], findfirst(==(1), x[end+1-n:end])), C)
 
     to_point_set = last.(A_with_j)
-    A = map.(Ref(first), 
-             filter(x->last(x)==j, A_with_j) for j in 1:n
-            )
+    A = map.(Ref(first), filter(x->last(x)==j, A_with_j) for j in 1:n)
 
     if M == nothing
         M = minkowski_sum(A...)
@@ -146,24 +142,31 @@ function minkowski_projection(
     for j in 1:nrows(cells)
         separated_row = filter.([x->to_point_set[x]==i for i in 1:n], Ref(row(cells, j)))
         labels_of_cell = Iterators.product(separated_row...) |> collect |> vec
+
         push!(projected_incidences, get.(Ref(labels), labels_of_cell, 0))
     end
 
-    return IncidenceMatrix(projected_incidences)
-end
-
-function fine_mixed_subdivisions(G::Graph{Directed})
-  A = vertices_of_newton_polytope(G)
-  return fine_mixed_subdivisions(A...)
+    return projected_incidences
 end
 
 @doc raw"""
-    fine_mixed_subdivisions(IncidenceMatrix, G::Graph{Directed})
+    fine_mixed_subdivisions(as::Type{T} = SubdivisionOfPoints, G::Graph{Directed})
 """
-function fine_mixed_subdivisions(::Type{IncidenceMatrix}, G::Graph{Directed})
+function fine_mixed_subdivisions(as::Type{T}, G::Graph{Directed}; project_full=true) where {
+  T<:Union{IncidenceMatrix, SubdivisionOfPoints, PolyhedralComplex}
+}
   A = vertices_of_newton_polytope(G)
-  return fine_mixed_subdivisions(IncidenceMatrix, A...)
+
+  if as == IncidenceMatrix
+      return fine_mixed_subdivisions(IncidenceMatrix, A...)
+  else 
+      return fine_mixed_subdivisions(as, A...; project_full=project_full)
+  end
 end
+fine_mixed_subdivisions(
+    G::Graph{Directed}; 
+    project_full=true
+) = fine_mixed_subdivisions(SubdivisionOfPoints, G; project_full=project_full)
 
 @doc raw"""
     fine_mixed_subdivisions(A::AbstractVector{<:PointVector}...)
@@ -196,26 +199,109 @@ end
 @doc raw"""
     fine_mixed_subdivisions(IncidenceMatrix, A::AbstractVector{<:PointVector}...)
 
-Gives a list of `IncidenceMatrix` for fine mixed subdivisions of the Minkowski sum 
+Gives a list of incidence matrices for fine mixed subdivisions of the Minkowski sum 
 of `A[1]`, `A[2]`, ..., `A[n]` together with the Minkowski sum itself.
 """
-function fine_mixed_subdivisions(::Type{IncidenceMatrix}, A::AbstractVector{<:PointVector}...)
-    M, subdivisions = mixed_subdivisions(A...)
+function fine_mixed_subdivisions(
+    ::Type{IncidenceMatrix}, 
+    A::AbstractVector{<:PointVector}...
+)
+    M, subdivisions = fine_mixed_subdivisions(A...)
 
     incidences = IncidenceMatrix.(subdivisions)
     for I in incidences
         resize!(I, nrows(I), length(M))
     end
-    return M, IncidenceMatrix.(subdivisions)
+    return M, incidences
 end
 
-function polytrope_duals(G::Graph{Directed}; project_full=false)
-  M,T = mixed_subdivisions(G)
+@doc raw"""
+    fine_mixed_subdivisions(SubdivisionOfPoints, A::AbstractVector{<:PointVector}...)
+
+Gives a list of fine mixed subdivisions of the Minkowski sum of `A[1]`, `A[2]`, ..., `A[n]` 
+as `SubdivisionOfPoints`.
+"""
+function fine_mixed_subdivisions(
+    ::Type{SubdivisionOfPoints},
+    A::AbstractVector{<:PointVector}...;
+    project_full=true
+)
+    M, subdivisions = fine_mixed_subdivisions(IncidenceMatrix, A...)
+    d = length(M[1])
+    Mmat = matrix(QQ, reduce(hcat, M) |> transpose)
+
+    if project_full
+      π = sparse_matrix(QQ)
+      for i in 2:d
+          push!(π, sparse_row(QQ, [(i,QQ(1))]))
+      end
+      π = matrix(π) |> transpose
+
+      return subdivision_of_points.(Ref(Mmat*π), subdivisions)
+    else
+      return subdivision_of_points.(Ref(Mmat), subdivisions)
+    end
+end
+
+@doc raw"""
+    fine_mixed_subdivisions(PolyhedralComplex, A::AbstractVector{<:PointVector}...)
+
+Gives a list of fine mixed subdivisions of the Minkowski sum of `A[1]`, `A[2]`, ..., `A[n]` 
+as `PolyhedralComplex`.
+"""
+function fine_mixed_subdivisions(
+  ::Type{PolyhedralComplex},
+  A::AbstractVector{<:PointVector}...;
+  project_full=true
+)
+  M, subdivisions = fine_mixed_subdivisions(IncidenceMatrix, A...)
+  
+  return fine_mixed_subdivisions(PolyhedralComplex, M, subdivisions; project_full=project_full)
+end
+
+function fine_mixed_subdivisions(
+        ::Type{PolyhedralComplex}, 
+        M::AbstractVector{<:PointVector}, 
+        subdivisions; 
+        project_full=true
+) 
+  Mmat = reduce(hcat, M) |> transpose
   d = length(M[1])
-  incidences = IncidenceMatrix.([filter(x->1∈x, t) for t in T])
-  for I in incidences
+
+  if project_full
+      π = sparse_matrix(QQ)
+      for i in 2:d
+          push!(π, sparse_row(QQ, [(i,QQ(1))]))
+      end
+      π = matrix(π) |> transpose
+
+      return polyhedral_complex.(subdivisions, Ref(Mmat*π))
+  else 
+      return polyhedral_complex.(subdivisions, Ref(Mmat))
+  end
+end
+
+function polytrope_face_figures(::Type{IncidenceMatrix}, G::Graph{Directed})
+  A = vertices_of_newton_polytope(G)
+
+  M,T = fine_mixed_subdivisions(A...)
+
+  polytrope_filters = filter.(Ref(cell->1 in cell), T) .|> IncidenceMatrix
+  for I in polytrope_filters
       resize!(I, nrows(I), length(M))
   end
+
+  return M, polytrope_filters
+end
+function polytrope_face_figures(G; project_full=true)
+    return polytrope_face_figures(SubdivisionOfPoints, G; project_full=project_full)
+end
+
+function polytrope_face_figures(
+  ::Type{SubdivisionOfPoints}, G::Graph{Directed}; project_full=true
+)
+  M, polytrope_filters = polytrope_face_figures(IncidenceMatrix, G)
+  d = length(M[1])
 
   Mmat = matrix(QQ, reduce(hcat, M) |> transpose)
   if project_full
@@ -225,30 +311,29 @@ function polytrope_duals(G::Graph{Directed}; project_full=false)
       end
       π = matrix(π) |> transpose
 
-      return polyhedral_complex.(incidences, Ref(Mmat*π))
+      return subdivision_of_points.(Ref(Mmat*π), polytrope_filters)
   else 
-      return polyhedral_complex.(incidences, Ref(Mmat))
+      return subdivision_of_points.(Ref(Mmat), polytrope_filters)
   end
 end
 
-function mixed_subdivisions_as_complexes(M::AbstractVector{<:PointVector}, subdivisions; project_full=true) 
-  Mmat = reduce(hcat, M) |> transpose
+function polytrope_face_figures(
+  ::Type{PolyhedralComplex}, G::Graph{Directed}; project_full=true
+)
+  M, polytrope_filters = polytrope_face_figures(IncidenceMatrix, G)
+  d = length(M[1])
 
-  complexes = polyhedral_complex.(subdivisions, Ref(Mmat))
+  Mmat = matrix(QQ, reduce(hcat, M) |> transpose)
   if project_full
-      output = PolyhedralComplex[]
-      for P in complexes
-          maximal_polyhedra(P)
-          push!(output, P.pm_complex |> Polymake.polytope.project_full |> polyhedral_complex)
+      π = sparse_matrix(QQ)
+      for i in 2:d
+          push!(π, sparse_row(QQ, [(i,QQ(1))]))
       end
+      π = matrix(π) |> transpose
 
-      return output
-  else return complexes
+      return polyhedral_complex.(polytrope_filters, Ref(Mmat*π))
+  else 
+      return polyhedral_complex.(polytrope_filters, Ref(Mmat))
   end
-  #return subdivision_of_points.(Ref(Mmat), subdivisions)
 end
 
-mixed_subdivisions_as_complexes(
-  A::AbstractVector{<:AbstractVector{T}}...;
-  project_full=true
-) where {T} = mixed_subdivisions_as_complexes(mixed_subdivisions(IncidenceMatrix, A...); project_full=true)
