a31112e4e16b2c4637149043fc2a23b8e526b12e
Kamillo Ferry <kafe@kafe.dev>
2024-11-15 02:39:59 +0100

    Cleanup the code

--- examples/intro.jl
+++
@@ -1,9 +0,0 @@
-using Oscar
-using Polytropes
-
-G = complete_dag(3)
-I = toric_ideal(G)
-# GF,gbs = groebner_fan(I; return_groebner_bases=true)
-
-# Needs https://github.com/ooinaruhugh/Oscar.jl/tree/kf/initial-ideals-from-groebner-fan
-GF,gbs,ords,initial_ideals = groebner_fan(I; return_groebner_bases=true,return_orderings=true,return_initial_ideals=true)
