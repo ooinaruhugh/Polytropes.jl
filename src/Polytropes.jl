435e96278f3fbccea8681ffeeaad3cccbce9db81
Kamillo Ferry <kafe@kafe.dev>
2025-01-28 00:33:10 +0100

    Cleaner WDP

--- src/Polytropes.jl
+++ src/Polytropes.jl
@@ -2,10 +2,12 @@ module Polytropes
 
 import Oscar
 
-include("graphs.jl")
-include("polyhedra.jl")
+include("Graphs.jl")
+include("ToricIdeal.jl")
+include("weighted_digraph_polyhedron.jl")
+include("groebner_fan.jl")
 
-include("cayley-trick.jl")
+include("Cayley.jl")
 
 include("Transitivity.jl")
 import .Transitivity: transitive_closure, transitive_reduction
@@ -14,6 +16,7 @@ export transitive_reduction
 export transitively_closed_acyclic_graphs
 
 export complete_dag
+export complete_directed_graph
 
 export root_polytope
 export fundamental_polytope
@@ -21,6 +24,10 @@ export fundamental_polytope
 export opposite_graph
 export indegree
 export outdegree
+
+# export edge_ring
+#export edge_ring_inclusion
+# export all_pairs_shortest_path_ideal
 
 export weighted_digraph_polyhedron
 
