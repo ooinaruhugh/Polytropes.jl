425a9af043890fa48034905f529611f85d3116fd
Kamillo Ferry <kafe@kafe.dev>
2025-07-01 15:09:07 +0200

    Add variant of script for mptopcom with open-mpi

---
+++ examples/enumerate-with-mptopcom-mpi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+N=$1
+
+if [ ! -d tmp/$N ]; then
+  mkdir -p tmp/$N
+  julia --project examples/with-mptopcom.jl $N
+fi
+
+time (for file in tmp/$N/*.topcom; do mpirun -np $(nproc) mptopcom --central < $file 2> /dev/null | wc -l ; printf "{$file}\n" 1>&2; done) > $N.counts
+awk '{s+=$1} END {print s+1}' $N.counts
