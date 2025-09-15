3ba4e00e08c70cf5eca4cc8a7e983c4139e12a4f
Kamillo Ferry <kafe@kafe.dev>
2025-09-15 20:56:53 +0200

    Fix download-data.sh

--- data/complete-dags/download-data.sh
+++ data/complete-dags/download-data.sh
@@ -2,6 +2,6 @@ URL="https://zenodo.org/records/17123108/files/complete-dags.tar.xz?download=1"
 WORKDIR=$(mktemp -d)
 
 curl $URL -o $WORKDIR/data-supplement.tar.xz
-unzip $WORKDIR/data-supplement.zip -d $WORKDIR
+tar xJf $WORKDIR/data-supplement.tar.xz -C $WORKDIR/
 
 mv $WORKDIR/complete-dags/* .
