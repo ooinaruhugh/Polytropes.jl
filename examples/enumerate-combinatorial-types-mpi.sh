23d94031382169a9811e79b3aa69ae61a603450b
Kamillo Ferry <kafe@kafe.dev>
2025-09-15 18:41:59 +0200

    Add shell scripts

--- examples/enumerate-combinatorial-types-mpi.sh
+++ examples/enumerate-combinatorial-types-mpi.sh
@@ -7,8 +7,8 @@ count=`ls -1 data/combinatorial-types/$N/*.topcom | wc -l`
 echo
 
 for f in data/combinatorial-types/$N/*.topcom; do
-  mpirun -np $(nproc) mptopcom --central --regular <$f 2>/dev/null \
-    | pv -cl --name $(basename $f) | xz > data/combinatorial-types/$N/$(basename $f .topcom).out.xz
+  mpirun -np $(nproc) mptopcom --regular <$f 2>/dev/null \
+    | pv -cl --name $(basename $f) | xz > data/combinatorial-types/$N/$(basename $f .topcom).regular.out.xz
   printf "{$f}\n" 
 done | pv -cl --size $count --name Total > /dev/null
 
--- examples/enumerate-with-mptopcom-mpi.sh
+++ examples/enumerate-with-mptopcom-mpi.sh
@@ -2,10 +2,5 @@
 
 N=$1
 
-if [ ! -d tmp/$N ]; then
-  mkdir -p tmp/$N
-  julia --project examples/with-mptopcom.jl $N
-fi
-
 time (for file in tmp/$N/*.topcom; do mpirun -np $(nproc) mptopcom --central --regular < $file 2> /dev/null | wc -l ; printf "{$file}\n" 1>&2; done) > $N.counts
 awk '{s+=$1} END {print s+1}' $N.counts
